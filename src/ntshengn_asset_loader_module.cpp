0ead272eb3b6a5d55fbf98edf128f111aa1e8a5a
ntsh-oni <teamnutshell.dev@gmail.com>
2024-09-23 14:22:25 +0200

    remove warning ignore on Clang

--- src/ntshengn_asset_loader_module.cpp
+++ src/ntshengn_asset_loader_module.cpp
@@ -10,16 +10,11 @@
 #if defined(NTSHENGN_COMPILER_GCC)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wstringop-overflow"
-#elif defined(NTSHENGN_COMPILER_CLANG)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wstringop-overflow"
 #endif
 #define STB_IMAGE_IMPLEMENTATION
 #include "../external/stb/stb_image.h"
 #if defined(NTSHENGN_COMPILER_GCC)
 #pragma GCC diagnostic pop
-#elif defined(NTSHENGN_COMPILER_CLANG)
-#pragma clang diagnostic pop
 #endif
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "../external/stb/stb_truetype.h"
