0ae26fdbb9f2d7572f0698574bdcd8ed20fbc718
ntsh-oni <teamnutshell.dev@gmail.com>
2025-01-22 18:35:31 +0100

    change ttf loading to correctly calculate image size to fit font glyphs

--- src/ntshengn_asset_loader_module.cpp
+++ src/ntshengn_asset_loader_module.cpp
@@ -568,39 +568,73 @@ std::unordered_map<std::string, NtshEngn::Material> NtshEngn::AssetLoaderModule:
 }
 
 void NtshEngn::AssetLoaderModule::loadFontTtf(const std::string& filePath, float fontHeight, Font& font) {
-	const int width = 512;
-	const int height = 512;
-	stbtt_bakedchar backedChars[96];
+	stbtt_fontinfo fontInfo;
 
-	unsigned char pixels[512 * 512];
+	int firstCharacter = 32;
+	int lastCharacter = 128;
+
 	std::string fileContent = File::readBinary(filePath);
-	stbtt_BakeFontBitmap(reinterpret_cast<const unsigned char*>(fileContent.c_str()), 0, fontHeight, pixels, width, height, 32, 96, backedChars);
+	stbtt_InitFont(&fontInfo, reinterpret_cast<const unsigned char*>(fileContent.c_str()), 0);
+	float scale = stbtt_ScaleForPixelHeight(&fontInfo, fontHeight);
+
+	int ascent;
+	int descent;
+	stbtt_GetFontVMetrics(&fontInfo, &ascent, &descent, nullptr);
+
+	int width = 0;
+	for (int codepoint = firstCharacter; codepoint < lastCharacter; codepoint++) {
+		int x0;
+		int y0;
+		int x1;
+		int y1;
+		stbtt_GetCodepointBitmapBox(&fontInfo, codepoint, scale, scale, &x0, &y0, &x1, &y1);
+
+		width += (x1 - x0) + 1;
+	}
+
+	int height = static_cast<int>(scale * static_cast<float>(ascent - descent));
+
+	float inverseWidth = 1.0f / static_cast<float>(width);
+	float inverseHeight = 1.0f / static_cast<float>(height);
 
 	Image* fontImage = assetManager->createImage();
-	fontImage->width = 512;
-	fontImage->height = 512;
+	fontImage->width = static_cast<uint32_t>(width);
+	fontImage->height = static_cast<uint32_t>(height);
 	fontImage->format = ImageFormat::R8;
 	fontImage->colorSpace = ImageColorSpace::Linear;
-	fontImage->data.resize(512 * 512);
-	std::copy(pixels, pixels + (width * height), fontImage->data.begin());
+	fontImage->data.resize(width * height);
 
 	font.image = fontImage;
 	font.imageSamplerFilter = ImageSamplerFilter::Linear;
+	float xpos = 0.0f;
+	int xoff = 1;
+	for (int codepoint = firstCharacter; codepoint < lastCharacter; codepoint++) {
+		int x0;
+		int y0;
+		int x1;
+		int y1;
+		stbtt_GetCodepointBitmapBox(&fontInfo, codepoint, scale, scale, &x0, &y0, &x1, &y1);
 
-	for (unsigned char c = 32; c < 128; c++) {
-		float x = 0.0f;
-		float y = 0.0f;
-		stbtt_aligned_quad alignedQuad;
-		stbtt_GetBakedQuad(backedChars, width, height, static_cast<int>(c) - 32, &x, &y, &alignedQuad, 1);
+		int gw = x1 - x0;
+		int gh = y1 - y0;
+		stbtt_MakeCodepointBitmap(&fontInfo, fontImage->data.data() + (xoff + width), gw, gh, width, scale, scale, codepoint);
+
+		int advanceWidth;
+		stbtt_GetCodepointHMetrics(&fontInfo, codepoint, &advanceWidth, nullptr);
+
+		float xadvance = scale * static_cast<float>(advanceWidth);
+		xpos += xadvance;
 
 		FontGlyph glyph;
-		glyph.positionTopLeft = { alignedQuad.x0, alignedQuad.y0 };
-		glyph.positionBottomRight = { alignedQuad.x1, alignedQuad.y1 };
-		glyph.positionAdvance = backedChars[static_cast<int>(c) - 32].xadvance;
-		glyph.uvTopLeft = { alignedQuad.s0, alignedQuad.t0 };
-		glyph.uvBottomRight = { alignedQuad.s1, alignedQuad.t1 };
+		glyph.positionTopLeft = { static_cast<float>(x0), static_cast<float>(y0) };
+		glyph.positionBottomRight = { static_cast<float>(x1), static_cast<float>(y1) };
+		glyph.positionAdvance = xadvance;
+		glyph.uvTopLeft = { static_cast<float>(xoff) * inverseWidth, inverseHeight };
+		glyph.uvBottomRight = { static_cast<float>(xoff + gw) * inverseWidth, static_cast<float>(1 + gh) * inverseHeight };
+		
+		font.glyphs[static_cast<char>(codepoint)] = glyph;
 
-		font.glyphs[c] = glyph;
+		xoff += gw + 1;
 	}
 }
 
